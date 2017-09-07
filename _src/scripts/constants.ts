fb31d40b86a7970b0d61d90a63600473e1e9b931
Maxim <mgreen097@gmail.com>
2017-09-08 01:09:51 +0300

    added CONTAINS_PX_OR_EM constat

--- _src/scripts/constants.ts
+++ _src/scripts/constants.ts
@@ -1,6 +1,7 @@
 export const HALF = 0.5;
 export const BROWSER_VIEWPORT_WIDTH = '100vw';
 export const BROWSER_DEFAULT_FONT_SIZE = 16;
-export const CONTAINS_PX = /^\d+px$/;
-export const CONTAINS_REM = /^\d+rem$/;
-export const CONTAINS_EM = /^\d+em$/;
+export const CONTAINS_PX = /\d+px/;
+export const CONTAINS_REM = /\d+rem/;
+export const CONTAINS_EM = /\d+em/;
+export const CONTAINS_PX_OR_EM = /\d+(px|em)/;
