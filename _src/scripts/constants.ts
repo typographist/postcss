5e7e5ec20fb5c041db765fdab57c235253a62e45
Maxim <mgreen097@gmail.com>
2017-09-08 11:34:36 +0300

    fixed constants

--- _src/scripts/constants.ts
+++ _src/scripts/constants.ts
@@ -1,7 +1,8 @@
 export const HALF = 0.5;
 export const BROWSER_VIEWPORT_WIDTH = '100vw';
 export const BROWSER_DEFAULT_FONT_SIZE = 16;
-export const CONTAINS_PX = /\d+px/;
+export const CONTAINS_PX = /\d+(?:[\.]\d+)?px/;
 export const CONTAINS_REM = /\d+rem/;
-export const CONTAINS_EM = /\d+em/;
-export const CONTAINS_PX_OR_EM = /\d+(px|em)/;
+export const CONTAINS_EM = /\d+(?:[\.]\d+)?em/;
+export const CONTAINS_PX_OR_EM = /\d+(?:[\.]\d+)?(px|em)/;
+export const FLOATING_POINT_NUMBER = /^\d+(?:[\.]\d+)?$/;
