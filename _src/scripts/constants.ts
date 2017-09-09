d4bc8703eb3dda2e2627f5ea7e0aa02625481edf
Maxim <mgreen097@gmail.com>
2017-09-10 02:29:36 +0300

    added constants

--- _src/scripts/constants.ts
+++ _src/scripts/constants.ts
@@ -1,8 +1,11 @@
 export const HALF = 0.5;
 export const BROWSER_VIEWPORT_WIDTH = '100vw';
 export const BROWSER_DEFAULT_FONT_SIZE = 16;
-export const CONTAINS_PX = /\d+(?:[\.]\d+)?px/;
-export const CONTAINS_REM = /\d+rem/;
-export const CONTAINS_EM = /\d+(?:[\.]\d+)?em/;
-export const CONTAINS_PX_OR_EM = /\d+(?:[\.]\d+)?(px|em)/;
-export const FLOATING_POINT_NUMBER = /^\d+(?:[\.]\d+)?$/;
+export const CONTAINS_PX = /[+-]?\d+(\.\d+)?px/;
+export const CONTAINS_REM = /[+-]?\d+(\.\d+)?rem/;
+export const CONTAINS_EM = /[+-]?\d+(\.\d+)?em/;
+export const CONTAINS_PX_OR_EM = /[-+]?\d+(\.\d+)?(px|em)/;
+export const FLOATING_POINT_NUMBER = /[-+]?\d+(\.\d+)?/;
+export const CONTAINS_AT = /[-+]?\d+(\.\d+)?(px|em) at \d+/;
+export const CONTAINS_FONT_SIZE = /\b[-+]?\d+(\.\d+)?(px|em)\b/gi;
+export const CONTAINS_TARGET = /\b\d+\b/g;
