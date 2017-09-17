7d2adf875fa5f4388994eaa0b781498b6734e22d
Maxim <mgreen097@gmail.com>
2017-09-17 23:10:54 +0300

    created ratios.js

---
+++ api/ratios.js
@@ -0,0 +1,19 @@
+export const DOUBLE_OCTAVE = 4;
+export const PI = 3.14159265359;
+export const MAJOR_TWELFTH = 3;
+export const MAJOR_ELEVENTH = 2.666666667;
+export const MAJOR_TENTH = 2.5;
+export const OCTAVE = 2;
+export const MAJOR_SEVENTH = 1.875;
+export const MINOR_SEVENTH = 1.777777778;
+export const MAJOR_SIXTH = 1.666666667;
+export const PHI = 1.618034;
+export const GOLDEN = 1.618034;
+export const FIFTH = 1.5;
+export const AUGMENTED_FOURTH = 1.41421;
+export const FOURTH = 1.333333333;
+export const MAJOR_THIRD =  1.25;
+export const MINOR_THIRD = 1.2;
+export const MAJOR_SECOND = 1.125;
+export const MINOR_SECOND = 1.066666667;
+
