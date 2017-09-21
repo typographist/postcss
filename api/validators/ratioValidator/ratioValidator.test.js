8c80c80667a0203ccdc3bed6a9235de46d016cd4
Maxim <mgreen097@gmail.com>
2017-09-22 01:34:24 +0300

    added ratioValidator.js

---
+++ api/validators/ratioValidator/ratioValidator.test.js
@@ -0,0 +1,26 @@
+import { isRatioContainsAt, isValidRatio } from './ratioValidator';
+
+describe('isRatioContainsAt', () => {
+  it('should be contains word at in entry string', () => {
+    expect(isRatioContainsAt('12px at 8')).toBe(true);
+  });
+
+  it('should be not contains word at in string', () => {
+    expect(isRatioContainsAt('this is incorrect string')).toBe(false);
+  });
+});
+
+
+describe('isValidRatio', () => {
+  it('should typeof ratio is number', () => {
+    expect(isValidRatio(11111)).toBe(true);
+  });
+
+  it('should be typeof ratio is string and contains word at', () => {
+    expect(isValidRatio('5678px at 8.888')).toBe(true);
+  });
+
+  it('should be typeof ratio is string and contains word at', () => {
+    expect(isValidRatio(NaN)).toBe(false);
+  });
+});
