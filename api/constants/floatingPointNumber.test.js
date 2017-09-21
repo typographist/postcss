11f91baa4823bb1a6bfcffc53946362aa093152b
Maxim <mgreen097@gmail.com>
2017-09-22 01:32:23 +0300

    added floatingPoint.test

---
+++ api/constants/floatingPointNumber.test.js
@@ -0,0 +1,23 @@
+import { FLOATING_POINT_NUMBER } from '../constants';
+
+describe('FLOATING_POINT_NUMBER', () => {
+  test('integer support', () => {
+    expect(FLOATING_POINT_NUMBER.test('12')).toBe(true);
+  });
+
+  test('floating points support', () => {
+    expect(FLOATING_POINT_NUMBER.test('1.2')).toBe(true);
+  });
+
+  test('negative meaning', () => {
+    expect(FLOATING_POINT_NUMBER.test('-7')).toBe(true);
+  });
+
+  test('negative floating-point value', () => {
+    expect(FLOATING_POINT_NUMBER.test('-7.777')).toBe(true);
+  });
+
+  test('not a number', () => {
+    expect(FLOATING_POINT_NUMBER.test('myNameIsMax')).toBe(false);
+  });
+});
