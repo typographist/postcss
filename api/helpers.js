ea96f33fa35da1f4d8b123ec1e3d29635508f407
Maxim <mgreen097@gmail.com>
2017-09-22 02:20:36 +0300

    fixed helpers.js

--- api/helpers.js
+++ api/helpers.js
@@ -6,45 +6,6 @@ import * as constants from './constants';
 //   return null;
 // };
 
-export const calcLeading = (base, lineHeight) => {
-  if (Array.isArray(base)) {
-    return Math.round(base[0] * lineHeight);
-  }
-  return Math.round(base * lineHeight);
-};
-
-export const calcRoot = val => (
-  Math.round(val * constants.HALF)
-);
-
-export const convertToEm = val => (
-  `${val / constants.BROWSER_DEFAULT_FONT_SIZE}em`
-);
-
-export const convertToPx = val => (
-  `${val / constants.BROWSER_DEFAULT_FONT_SIZE}px`
-);
-
-export const makeArray = length => (
-  Array.from({ length }, (item, i) => i)
-);
-
-export const deepFind = (obj, key, memo) => {
-  let result = memo;
-  if (!isArray(memo)) result = [];
-
-  for (let i in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, i)) {
-      if (i === key) {
-        result.push(obj[i]);
-      } else if (isArray(obj[i]) || isObject(obj[i])) {
-        deepFind(obj[i], key, result);
-      }
-    }
-  }
-
-  return result;
-};
 
 export const getValidBreakpoint = (val) => {
   let result;
