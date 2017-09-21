e95b5dccd131d7c39e35a95c8549b4a914920020
Maxim <mgreen097@gmail.com>
2017-09-22 02:19:49 +0300

    created regex folder

---
+++ api/regex/regex.js
@@ -0,0 +1,8 @@
+export const FLOATING_POINT_NUMBER = /-?\b\d+(\.\d+)?/;
+export const CONTAINS_PX = /-?\b\d+(\.\d+)?px/;
+export const CONTAINS_REM = /-?\b\d+(\.\d+)?rem/;
+export const CONTAINS_EM = /-?\b\d+(\.\d+)?em/;
+export const CONTAINS_PX_OR_EM = /-?\b\d+(\.\d+)?(px|em)/;
+export const CONTAINS_AT = /-?\b\d+(\.\d+)?(px|em) at -?\d+(\.\d+)??\b/;
+export const CONTAINS_FONT_SIZE = /\b-?\d+(\.\d+)?(px|em)\b/gi;
+export const CONTAINS_TARGET = /-?\b\d+(\.\d+)?\b\s*$/g;
