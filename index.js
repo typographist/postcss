652ba77c664b71d6b2ddd977cdecf5e4da0fa242
Maxim <mgreen097@gmail.com>
2017-09-25 20:02:03 +0300

    fixed index.js

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-import isConfigValid from './api/isConfigValid';
+// import isConfigValid from './api/isConfigValid';
 
-const config = {
-  base: '16px',
-  lineHeight: 1.5,
-  ratio: '45px at 6',
-  tablet: {
-    breakpoint: '640px',
-    base: '17px',
-  },
-  desktop: {
-    breakpoint: '1024px',
-    base: '18px',
-    lineHeight: 1.7,
-    ratio: 1.333,
-  },
-  lgDesktop: {
-    breakpoint: '1200px',
-    base: '20px',
-  },
-  xlDesktop: {
-    breakpoint: '1600px',
-    base: '22px',
-  },
-};
+// const config = {
+//   base: '16px',
+//   lineHeight: 1.5,
+//   ratio: '45px at 6',
+//   tablet: {
+//     breakpoint: '640px',
+//     base: '17px',
+//   },
+//   desktop: {
+//     breakpoint: '1024px',
+//     base: '18px',
+//     lineHeight: 1.7,
+//     ratio: 1.333,
+//   },
+//   lgDesktop: {
+//     breakpoint: '1200px',
+//     base: '20px',
+//   },
+//   xlDesktop: {
+//     breakpoint: '1600px',
+//     base: '22px',
+//   },
+// };
 
-nt(6);
+// nt(6);
 
-console.log(isConfigValid(config);
-
+// console.log(isConfigValid(config));
