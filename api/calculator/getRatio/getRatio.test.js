f42dc367324b6ee1ff529fec93be4fd58b235143
Maxim <mgreen097@gmail.com>
2017-09-28 20:44:29 +0300

    created  getRatio.test

---
+++ api/calculator/getRatio/getRatio.test.js
@@ -0,0 +1,30 @@
+import { getFontSizeFromRatio, getTargetFromRatio, calcRatio, getRatio } from './getRatio';
+
+describe('getFontSizeFromRatio', () => {
+  it('should get font size from ratio', () => {
+    expect(getFontSizeFromRatio('45.5px at 7')).toBe('45.5px');
+  });
+});
+
+
+describe('getTargetFromRatio', () => {
+  it('should get target from ratio', () => {
+    expect(getTargetFromRatio('45.5px at 7')).toBe(7);
+  });
+});
+
+describe('calcRatio', () => {
+  it('should calc ratio', () => {
+    expect(calcRatio(45, 16, 6)).toBe(1.18809);
+  });
+});
+
+describe('getRatio', () => {
+  it('should if ratio is string', () => {
+    expect(getRatio('45px at 6', 16)).toBe(1.18809);
+  });
+
+  it('should if ratio is number', () => {
+    expect(getRatio(1.5)).toBe(1.5);
+  });
+});
