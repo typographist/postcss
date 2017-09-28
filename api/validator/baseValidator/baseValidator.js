95db8aab973393555dd7fd64ca6ff3683c217c14
Maxim <mgreen097@gmail.com>
2017-09-28 20:36:03 +0300

    created baseValidator

---
+++ api/validator/baseValidator/baseValidator.js
@@ -0,0 +1,48 @@
+import { CONTAINS_PX_OR_EM } from '../../regex';
+
+/**
+ * @param {any} checkBase
+ * @return {boolean} 
+ */
+export const isBaseString = (checkBase) => {
+  try {
+    switch (typeof checkBase) {
+      case 'string':
+        return true;
+      default:
+        throw new Error('Base value must be a string or Array<string>!');
+    }
+  } catch (err) {
+    console.log(err.message);
+    return false;
+  }
+};
+
+
+/**
+ * @param {string} checkBase 
+ * @return {boolean} 
+ */
+export const isBaseContainPxOrEm = (checkBase) => {
+  try {
+    switch (CONTAINS_PX_OR_EM.test(checkBase)) {
+      case true:
+        return true;
+      default:
+        throw new Error(`${checkBase} is incorrect value! Please, use pixels or em.`);
+    }
+  } catch (err) {
+    console.log(err.message);
+    return false;
+  }
+};
+
+
+/**
+ * @param {array<any>} checkBases - flat array
+ * @param {function} fn
+ * @return {boolean} 
+ */
+export const isValidBases = (checkBases, fn) => (
+  checkBases.every(base => fn(base))
+);
