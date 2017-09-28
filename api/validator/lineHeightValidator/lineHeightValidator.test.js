cf5ee7954814dfe047a98d031879b2a0ac87f896
Maxim <mgreen097@gmail.com>
2017-09-28 20:34:47 +0300

    created lineHeightValidator.test

---
+++ api/validator/lineHeightValidator/lineHeightValidator.test.js
@@ -0,0 +1,11 @@
+import lineHeight from './lineHeightValidator';
+
+describe('LineHeight', () => {
+  it('should be array of numbers', () => {
+    expect(lineHeight([1, 2, 3, 4])).toBe(true);
+  });
+
+  it('should not be not array of numbers', () => {
+    expect(lineHeight(['fake', true, 123, {}])).toBe(false);
+  });
+});
