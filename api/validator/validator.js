4da8dc636e4b626d5b816264b21d4fdd2a1796a9
Maxim <mgreen097@gmail.com>
2017-09-28 20:36:23 +0300

    created validator

---
+++ api/validator/validator.js
@@ -0,0 +1,26 @@
+import flatten from '../helpers/flatten';
+import findAll from '../helpers/findAll';
+
+import { isBaseString, isBaseContainPxOrEm, isValidBases } from './baseValidator/baseValidator';
+import isValidLineHeights from './lineHeightValidator/lineHeightValidator';
+import { isValidRatios } from './ratioValidator/ratioValidator';
+import { breakpointIsString, isBreakpointContainsPxOrEm, isValidBreakpoints } from './breakpointsValidator/breakpointsValidator';
+
+
+const isValidConfig = (config) => {
+  const bases = flatten(findAll(config, 'base'));
+  const lineHeighs = findAll(config, 'lineHeight');
+  const ratios = findAll(config, 'ratio');
+  const breaks = findAll(config, 'breakpoint');
+
+  return [
+    isValidBases(bases, isBaseString),
+    isValidBases(bases, isBaseContainPxOrEm),
+    isValidLineHeights(lineHeighs),
+    isValidRatios(ratios),
+    isValidBreakpoints(breaks, breakpointIsString),
+    isValidBreakpoints(breaks, isBreakpointContainsPxOrEm),
+  ].every(Boolean);
+};
+
+export default isValidConfig;
