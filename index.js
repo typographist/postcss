ef0d7422d5e3ed2ba698f6c8459c53630b79d94b
Maxim <mgreen097@gmail.com>
2017-10-04 23:06:36 +0300

    added calculator

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 // import isConfigValid from './api/isConfigValid';
 
-// const config = {
-//   base: '16px',
-//   lineHeight: 1.5,
-//   ratio: '45px at 6',
-//   tablet: {
-//     breakpoint: '640px',
-//     base: '17px',
-//   },
-//   desktop: {
-//     breakpoint: '1024px',
-//     base: '18px',
-//     lineHeight: 1.7,
-//     ratio: 1.333,
-//   },
-//   lgDesktop: {
-//     breakpoint: '1200px',
-//     base: '20px',
-//   },
-//   xlDesktop: {
-//     breakpoint: '1600px',
-//     base: '22px',
-//   },
-// };
+import calculator from './api/calculator';
+
+const config = {
+  base: ['16px', '33px'],
+  lineHeight: 1.5,
+  ratio: '45px at 6',
+  tablet: {
+    breakpoint: '640px',
+    base: '17px',
+  },
+  desktop: {
+    breakpoint: '1024px',
+    base: '18px',
+    lineHeight: 1.7,
+    ratio: 1.333,
+  },
+  lgDesktop: {
+    breakpoint: '1200px',
+    base: '20px',
+  },
+  xlDesktop: {
+    breakpoint: '1600px',
+    base: '22px',
+  },
+};
+
+console.log(calculator(config));
 
 // nt(6);
 
