57b8f54293e862ce73d2f7def19b64149ac2a016
Maxim <mgreen097@gmail.com>
2017-10-04 23:11:49 +0300

    added tests in breakpointsValidator.test.js

--- api/validator/breakpointsValidator/breakpointsValidator.test.js
+++ api/validator/breakpointsValidator/breakpointsValidator.test.js
@@ -1,5 +1,55 @@
 import * as validators from './breakpointsValidator';
 
+const config = {
+  base: ['16px', '33px'],
+  lineHeight: 1.5,
+  ratio: '45px at 6',
+  tablet: {
+    breakpoint: '640px',
+    base: '17px',
+  },
+  desktop: {
+    breakpoint: '1024px',
+    base: '18px',
+    lineHeight: 1.7,
+    ratio: 1.333,
+  },
+  lgDesktop: {
+    breakpoint: '1200px',
+    base: '20px',
+  },
+  xlDesktop: {
+    breakpoint: '1600px',
+    base: '22px',
+  },
+};
+
+const breakpoints = [
+  {
+    breakpoint: '640px',
+    base: '17px',
+  },
+  {
+    breakpoint: '1024px',
+    base: '18px',
+    lineHeight: 1.7,
+    ratio: 1.333,
+  },
+  {
+    breakpoint: '1200px',
+    base: '20px',
+  },
+  {
+    breakpoint: '1600px',
+    base: '22px',
+  },
+];
+
+const breakpoint = {
+  breakpoint: '640px',
+  base: '17px',
+};
+
 describe('breakpointIsString', () => {
   it('should be if string', () => {
     expect(validators.breakpointIsString('bam baaaam!')).toBe(true);
@@ -38,5 +88,50 @@ describe('isValidBreakpoints', () => {
       ['12px', '34px', '56em'],
       validators.isBreakpointContainsPxOrEm,
     )).toBe(true);
+  });
+
+  it('should if all breakpoints contains breakpoint key', () => {
+    expect(validators.isValidBreakpoints(
+      breakpoints,
+      validators.checkContainsBreakpointKey,
+    )).toBe(true);
+  });
+
+
+  describe('getBreakpoints', () => {
+    it('should get breakpoints', () => {
+      expect(validators.getBreakpoints(config)).toEqual([
+        {
+          breakpoint: '640px',
+          base: '17px',
+        },
+        {
+          breakpoint: '1024px',
+          base: '18px',
+          lineHeight: 1.7,
+          ratio: 1.333,
+        },
+        {
+          breakpoint: '1200px',
+          base: '20px',
+        },
+        {
+          breakpoint: '1600px',
+          base: '22px',
+        },
+      ]);
+    });
+  });
+
+
+  describe('checkContainsBreakpointKey', () => {
+    it('should if is breakpoint contains breakpoint key', () => {
+      expect(validators.checkContainsBreakpointKey(breakpoint)).toBe(true);
+    });
+  });
+
+
+  describe('', () => {
+
   });
 });
