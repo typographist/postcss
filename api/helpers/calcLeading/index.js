7e5372fa5998a31e84034aff1b80e6699b799320
Maxim <mgreen097@gmail.com>
2017-10-07 16:40:43 +0300

    created caclLeading.test.js

--- api/helpers/calcLeading/index.js
+++ api/helpers/calcLeading/index.js
@@ -1,7 +1,13 @@
+/**
+ * @param {number|array<number>} base 
+ * @param {number} lineHeight
+ * @return {number} 
+ */
 const calcLeading = (base, lineHeight) => {
   if (Array.isArray(base)) {
     return Math.round(base[0] * lineHeight);
   }
+  
   return Math.round(base * lineHeight);
 };
 
