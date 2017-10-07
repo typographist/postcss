5f20c81e64986b73b4028d4a40f211a9dc56fb1a
Maxim <mgreen097@gmail.com>
2017-10-07 18:10:37 +0300

    fixed index.js

--- index.js
+++ index.js
@@ -1,33 +1,14 @@
-// import isConfigValid from './api/isConfigValid';
+import postcss from 'postcss';
+import calculate from './_api/calculator';
 
-import calculator from './api/calculator';
-
-const config = {
-  base: ['16px', '33px'],
+const defualtConfig = {
+  base: '16px',
   lineHeight: 1.5,
-  ratio: '45px at 6',
-  tablet: {
-    breakpoint: '640px',
-    base: '17px',
-  },
-  desktop: {
-    breakpoint: '1024px',
-    base: '18px',
-    lineHeight: 1.7,
-    ratio: 1.333,
-  },
-  lgDesktop: {
-    breakpoint: '1200px',
-    base: '20px',
-  },
-  xlDesktop: {
-    breakpoint: '1600px',
-    base: '22px',
-  },
+  ratio: 1.333,
 };
 
-console.log(calculator(config));
+module.exports = postcss.plugin('new-typography', (config = defualtConfig) => {
+  const options = calculate(config);
 
-// nt(6);
-
-// console.log(isConfigValid(config));
+  console.log(options);
+});
