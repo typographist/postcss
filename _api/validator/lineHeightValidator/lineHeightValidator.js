eac5b10ff7c0ed57138b89d91a5c55cb59c9e020
Maxim <mgreen097@gmail.com>
2017-10-21 14:39:20 +0300

    fixed lineHeightValidator

--- _api/validator/lineHeightValidator/lineHeightValidator.js
+++ _api/validator/lineHeightValidator/lineHeightValidator.js
@@ -1,5 +1,4 @@
-// import isNumber from '../../helpers/isNumber/isNumber';
-import isNumeric from '../../helpers/isNumeric/';
+const isNumeric = require('../../helpers/isNumeric/');
 
 /**
  * Check whether all the values of the number?
@@ -11,5 +10,5 @@ const isValidLineHeights = lineHeights => (
   lineHeights.every(lineHeight => isNumeric(lineHeight))
 );
 
-export default isValidLineHeights;
+module.exports = isValidLineHeights;
 
