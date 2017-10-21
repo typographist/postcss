c686c8b8440d43748325ac52a28ef18a49fd51c8
Maxim <mgreen097@gmail.com>
2017-10-21 14:27:00 +0300

    fixed index in makeBrakpoints folder

--- _api/calculator/makeBreakpoints/index.js
+++ _api/calculator/makeBreakpoints/index.js
@@ -1,10 +1,10 @@
-import isObject from '../../helpers/isObject';
+const isObject = require('../../helpers/isObject');
 
 /**
  * @param {object} config
  * @return {array<string>}
  */
-export const getBreakpointsName = config => (
+const getBreakpointsName = config => (
   Object.keys(config).filter(key => (
     [
       key !== 'base',
@@ -19,7 +19,7 @@ export const getBreakpointsName = config => (
  * @param {object} config
  * @return {array<object>} array with breakpoints
  */
-export const getBreakpoints = config => (
+const getBreakpoints = config => (
   Object.values(config).filter(val => (
     isObject(val) && val.breakpoint
   ))
@@ -31,7 +31,7 @@ export const getBreakpoints = config => (
  * @param {function|array<any>} breakpointsName 
  * @return {array<object>} brakpoint with names
  */
-export const setBreakpointName = (breakpoints, breakpointsName) => (
+const setBreakpointName = (breakpoints, breakpointsName) => (
   breakpoints.map((breakpoint, i) => (
     {
       ...breakpoint,
@@ -45,7 +45,7 @@ export const setBreakpointName = (breakpoints, breakpointsName) => (
  * @param {function} breakpoints 
  * @return {array<object>} 
  */
-export const setBreakpointValue = breakpoints => (
+const setBreakpointValue = breakpoints => (
   breakpoints.map(breakpoint => (
     {
       ...breakpoint,
@@ -54,3 +54,9 @@ export const setBreakpointValue = breakpoints => (
   ))
 );
 
+module.exports = {
+  getBreakpointsName,
+  getBreakpoints,
+  setBreakpointName,
+  setBreakpointValue,
+};
