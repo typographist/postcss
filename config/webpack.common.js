49b9dca1dec7cedb41e46eba40b8fea3c48f61e0
Maxim <mgreen097@gmail.com>
2017-10-21 14:40:09 +0300

    fixed webpack.common

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,12 +1,8 @@
 import path from 'path';
-import HappyPack from 'happypack';
 import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-
-const happyThreadPool = HappyPack.ThreadPool({ size: 5 });
 
 export default {
-  entry: ['babel-polyfill', path.join(__dirname, '..', '/_parser/index')],
+  entry: ['babel-polyfill', path.join(__dirname, '..', '/index.js')],
   watchOptions: {
     argregateTimeout: 100,
   },
@@ -15,7 +11,12 @@ export default {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ['happypack/loader?id=js'],
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['env', 'stage-0'],
+          },
+        },
       },
     ],
   },
@@ -23,20 +24,5 @@ export default {
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Page',
-      template: path.join(__dirname, '..', '_src', 'index.html'),
-      filename: path.join(__dirname, '..', '_dist', 'index.html'),
-    }),
-    new HappyPack({
-      id: 'js',
-      threadPool: happyThreadPool,
-      loaders: [{
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'env', 'stage-0'],
-        },
-      }],
-    }),
   ],
 };
