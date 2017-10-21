180ff7b6f5acea0ad750920c735401977b30a6cc
Maxim <mgreen097@gmail.com>
2017-10-21 14:29:01 +0300

    fixed index in calcLeading

--- _api/helpers/calcLeading/index.js
+++ _api/helpers/calcLeading/index.js
@@ -7,8 +7,8 @@ const calcLeading = (base, lineHeight) => {
   if (Array.isArray(base)) {
     return Math.round(base[0] * lineHeight);
   }
-  
+
   return Math.round(base * lineHeight);
 };
 
-export default calcLeading;
+module.exports = calcLeading;
