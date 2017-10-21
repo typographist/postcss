f9cbb4f521d962bd7bd6f0099ad3acb131e07e66
Maxim <mgreen097@gmail.com>
2017-10-21 14:25:08 +0300

    fixed ratios

--- _api/ratios.js
+++ _api/ratios.js
@@ -1,19 +1,39 @@
-export const DOUBLE_OCTAVE = 4;
-export const PI = 3.14159265359;
-export const MAJOR_TWELFTH = 3;
-export const MAJOR_ELEVENTH = 2.666666667;
-export const MAJOR_TENTH = 2.5;
-export const OCTAVE = 2;
-export const MAJOR_SEVENTH = 1.875;
-export const MINOR_SEVENTH = 1.777777778;
-export const MAJOR_SIXTH = 1.666666667;
-export const PHI = 1.618034;
-export const GOLDEN = 1.618034;
-export const FIFTH = 1.5;
-export const AUGMENTED_FOURTH = 1.41421;
-export const FOURTH = 1.333333333;
-export const MAJOR_THIRD = 1.25;
-export const MINOR_THIRD = 1.2;
-export const MAJOR_SECOND = 1.125;
-export const MINOR_SECOND = 1.066666667;
+const DOUBLE_OCTAVE = 4;
+const PI = 3.14159265359;
+const MAJOR_TWELFTH = 3;
+const MAJOR_ELEVENTH = 2.666666667;
+const MAJOR_TENTH = 2.5;
+const OCTAVE = 2;
+const MAJOR_SEVENTH = 1.875;
+const MINOR_SEVENTH = 1.777777778;
+const MAJOR_SIXTH = 1.666666667;
+const PHI = 1.618034;
+const GOLDEN = 1.618034;
+const FIFTH = 1.5;
+const AUGMENTED_FOURTH = 1.41421;
+const FOURTH = 1.333333333;
+const MAJOR_THIRD = 1.25;
+const MINOR_THIRD = 1.2;
+const MAJOR_SECOND = 1.125;
+const MINOR_SECOND = 1.066666667;
 
+module.exports = {
+  DOUBLE_OCTAVE,
+  PI,
+  MAJOR_TWELFTH,
+  MAJOR_ELEVENTH,
+  MAJOR_TENTH,
+  OCTAVE,
+  MAJOR_SEVENTH,
+  MINOR_SEVENTH,
+  MAJOR_SIXTH,
+  PHI,
+  GOLDEN,
+  FIFTH,
+  AUGMENTED_FOURTH,
+  FOURTH,
+  MAJOR_THIRD,
+  MINOR_THIRD,
+  MAJOR_SECOND,
+  MINOR_SECOND,
+};
