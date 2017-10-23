dad463604f87dbaee196b10ff61dda5cf2dac58b
Maxim <mgreen097@gmail.com>
2017-10-23 20:02:06 +0300

    added function rootToPersentage

--- _parser/fontSizeDecl/index.js
+++ _parser/fontSizeDecl/index.js
@@ -1,9 +1,14 @@
 const postcss = require('postcss');
+const BROWSER_DEFAULT_FONT_SIZE = require('../../_api/constants').BROWSER_DEFAULT_FONT_SIZE;
+
+const rootToPersentage = fontSize => (
+  `${(fontSize / BROWSER_DEFAULT_FONT_SIZE) * 100}%`
+);
 
 const fontSizeDecl = size => (
   postcss.decl({
     prop: 'font-size',
-    value: size,
+    value: rootToPersentage(size),
   })
 );
 
