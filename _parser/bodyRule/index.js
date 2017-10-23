8d96d4c9db1c1e91fe633896939e063fee81b387
Maxim <mgreen097@gmail.com>
2017-10-23 19:45:48 +0300

    created bodyRule function

---
+++ _parser/bodyRule/index.js
@@ -0,0 +1,13 @@
+const postcss = require('postcss');
+const fontSizeDecl = require('../fontSizeDecl');
+
+const bodyRule = (fontSize) => {
+  const body = postcss.rule({
+    selector: 'body',
+  });
+  body.append(fontSizeDecl(fontSize));
+
+  return body;
+};
+
+module.exports = bodyRule;
