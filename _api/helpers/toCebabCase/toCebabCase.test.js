d15d8d1f00c9696c1efa5f6aea641fbc80804faa
Maxim <mgreen097@gmail.com>
2017-10-23 20:01:30 +0300

    deleted toCebabCase.test.js

--- _api/helpers/toCebabCase/toCebabCase.test.js
+++
@@ -1,7 +0,0 @@
-const toCebabCase = require('./');
-
-describe('toCebabCase', () => {
-  it('should convert to cebab case', () => {
-    expect(toCebabCase('camelCaseNotation')).toBe('camel-case-notation');
-  });
-});
