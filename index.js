0d5541ca6e2787a6da0908c6cdf620568818f308
Maxim <mgreen097@gmail.com>
2017-10-25 11:30:49 +0300

    fixed index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const postcss = require('postcss');
-const calculate = require('./_api/calculator');
-const fontSizeDecl = require('./_parser/fontSizeDecl');
-const rootRule = require('./_parser/rootRule');
-const bodyRule = require('./_parser/bodyRule');
-const mediaDecl = require('./_parser/mediaDecl');
-const variableDecl = require('./_parser/variableDecl');
-const rootToPersentage = require('./_parser/rootToPersentage');
-
+const calculate = require('./api/calculator');
+const rootSize = require('./parser/root/rootSize');
+const baseSize = require('./parser/base/baseSize');
 
 const defualtConfig = {
   base: '16px',
@@ -17,67 +12,24 @@ const defualtConfig = {
 const plugin = postcss.plugin('new-typography', (config = defualtConfig) => {
   const breakpoints = calculate(config);
 
-  const rootSize = (node) => {
-    const parent = node.parent;
-    if (parent && parent.selector !== ':root') {
-      node.remove();
-    } else {
-      const breakpoint = breakpoints.find(b => /^0/.test(b.value));
-
-      breakpoints
-        .reverse()
-        .filter(b => b.value !== '0px')
-        .map(b => node.parent.after(mediaDecl({
-          minWidth: b.value,
-          nestedRule: rootRule(b.root),
-        })));
-
-      breakpoints
-        .reverse()
-        .filter(b => b.value !== '0px')
-        .map(b => node.before(variableDecl({
-          name: b.name,
-          value: b.value,
-        })));
-
-      node.replaceWith(fontSizeDecl(breakpoint.root, rootToPersentage));
-    }
-  };
-
-  const baseSize = (node) => {
-    const parent = node.parent;
-    if (parent && parent.selector !== 'body') {
-      node.remove();
-    } else {
-      const breakpoint = breakpoints.find(b => /^0/.test(b.value));
-      breakpoints.reverse()
-        .filter(b => b.value !== '0px')
-        .map(b => node.parent.after(mediaDecl({
-          minWidth: b.value,
-          fontSize: b.root,
-          nestedRule: bodyRule(b.base),
-        })));
-
-      breakpoints.reverse()
-        .filter(b => b.value !== '0px')
-        .map(b => node.before(variableDecl({
-          name: b.name,
-          value: b.value,
-        })));
-      node.replaceWith(fontSizeDecl(breakpoint.root));
-    }
-  };
-
   return (root) => {
     root.walkAtRules((node) => {
       switch (node.name) {
-        case 'nt-root-size':
-          rootSize(node);
+        case 'nt-root':
+          rootSize(node, breakpoints);
           break;
 
-        case 'nt-base-size':
-          baseSize(node);
+        // case 'nt-root-fluid'
+        //   rootSizeFluid(node);
+        //   break;
+
+        case 'nt-base':
+          baseSize(node, breakpoints);
           break;
+
+        // case 'nt-base-fluid'
+        //   baseSizeFluid(node);
+        //   break;
 
         default:
           break;
