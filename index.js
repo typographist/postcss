0b655a8ce99ee6c3b8581110a14f57d668fa8abb
Maxim <mgreen097@gmail.com>
2017-10-29 14:32:53 +0300

    fixed index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const calculate = require('./api/calculator');
 const rootSize = require('./parser/root/rootSize');
 const baseSize = require('./parser/base/baseSize');
 const above = require('./parser/breakpoints/above');
-const ntFn = require('./parser/nt');
+const nt = require('./parser/ntFunction');
 const CONTAINS_NT_FUNCTION = require('./regex/').CONTAINS_NT_FUNCTION;
 
 const defualtConfig = {
@@ -15,7 +15,7 @@ const defualtConfig = {
 const plugin = postcss.plugin('new-typography', (config = defualtConfig) => {
   const breakpoints = calculate(config);
 
-  return (root, result) => {
+  return (root) => {
     root.walk((node) => {
       if (node.name === 'nt-root') {
         rootSize(node, breakpoints);
@@ -28,48 +28,11 @@ const plugin = postcss.plugin('new-typography', (config = defualtConfig) => {
       } else if (node.name === 'nt-between') {
         console.log('------ between');
       } else if (CONTAINS_NT_FUNCTION.test(node.value)) {
-        ntFn(node, breakpoints, root);
+        nt(node, breakpoints, root);
         console.log('------ nt');
       }
-      // switch (node.name) {
-      //   case 'nt-root':
-      //     rootSize(node, breakpoints);
-      //     break;
-
-      //   case 'nt-base':
-      //     baseSize(node, breakpoints);
-      //     break;
-
-      //   case 'nt-above':
-      //     above(node, breakpoints);
-      //     break;
-
-      //   case 'nt-below':
-      //     break;
-
-      //   case 'nt-between':
-      //     break;
-
-      //   default:
-      //     break;
-      // }
     });
   };
 });
 
 module.exports = plugin;
-
-// toDo
-/*
-1) Breakpoints
-  a) above
-  b) below
-  c) between
-
-2) Nt
-3) Css variables support
-4) Root fluid
-5) Base fluid
-6) Nt fluid
-7) Margins for headings
-*/
