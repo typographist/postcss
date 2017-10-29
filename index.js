6ae3f8285ebc2a5c900557a601f373b501ff56cd
Maxim <mgreen097@gmail.com>
2017-10-29 13:27:30 +0300

    fixed index

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const postcss = require('postcss');
 const calculate = require('./api/calculator');
 const rootSize = require('./parser/root/rootSize');
 const baseSize = require('./parser/base/baseSize');
+const above = require('./parser/breakpoints/above');
+const ntFn = require('./parser/nt');
+const CONTAINS_NT_FUNCTION = require('./regex/').CONTAINS_NT_FUNCTION;
 
 const defualtConfig = {
   base: '16px',
@@ -12,28 +15,44 @@ const defualtConfig = {
 const plugin = postcss.plugin('new-typography', (config = defualtConfig) => {
   const breakpoints = calculate(config);
 
-  return (root) => {
-    root.walkAtRules((node) => {
-      switch (node.name) {
-        case 'nt-root':
-          rootSize(node, breakpoints);
-          break;
-
-        // case 'nt-root-fluid'
-        //   rootSizeFluid(node);
-        //   break;
-
-        case 'nt-base':
-          baseSize(node, breakpoints);
-          break;
-
-        // case 'nt-base-fluid'
-        //   baseSizeFluid(node);
-        //   break;
-
-        default:
-          break;
+  return (root, result) => {
+    root.walk((node) => {
+      if (node.name === 'nt-root') {
+        rootSize(node, breakpoints);
+      } else if (node.name === 'nt-base') {
+        baseSize(node, breakpoints);
+      } else if (node.name === 'nt-above') {
+        above(node, breakpoints);
+      } else if (node.name === 'nt-below') {
+        console.log('------ below');
+      } else if (node.name === 'nt-between') {
+        console.log('------ between');
+      } else if (CONTAINS_NT_FUNCTION.test(node.value)) {
+        ntFn(node, breakpoints, root);
+        console.log('------ nt');
       }
+      // switch (node.name) {
+      //   case 'nt-root':
+      //     rootSize(node, breakpoints);
+      //     break;
+
+      //   case 'nt-base':
+      //     baseSize(node, breakpoints);
+      //     break;
+
+      //   case 'nt-above':
+      //     above(node, breakpoints);
+      //     break;
+
+      //   case 'nt-below':
+      //     break;
+
+      //   case 'nt-between':
+      //     break;
+
+      //   default:
+      //     break;
+      // }
     });
   };
 });
