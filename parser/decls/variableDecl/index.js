3790d0d2ffba5e1e96647f4de7154ebf85f976d5
Maxim <mgreen097@gmail.com>
2017-10-29 16:09:48 +0300

    fixed variableDecl

--- parser/decls/variableDecl/index.js
+++ parser/decls/variableDecl/index.js
@@ -1,12 +1,9 @@
 const postcss = require('postcss');
-
-const toCababCase = string => (
-  string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
-);
+const humps = require('humps');
 
 const variableDecl = ({ name, value }) => (
   postcss.decl({
-    prop: `--${toCababCase(name)}`,
+    prop: `--${humps.decamelize(name, { separator: '-' })}`,
     value,
   })
 );
