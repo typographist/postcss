c317b4c28ebd37ff23d8e5c35c1ecf5c3e41791a
maxinakenty <mgreen097@gmail.com>
2018-01-12 15:00:04 +0300

    created index.js for import all decls

---
+++ parser/decls/index.js
@@ -0,0 +1,11 @@
+const fontSizeDecl = require('./fontSizeDecl');
+const lineHeightDecl = require('./lineHeightDecl');
+const mediaDecl = require('./mediaDecl');
+const variableDecl = require('./variableDecl');
+
+module.exports = {
+  fontSizeDecl,
+  lineHeightDecl,
+  mediaDecl,
+  variableDecl,
+};
