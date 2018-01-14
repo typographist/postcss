0f54c22a2d0623f13aab0f89869e61a9fe6ea084
maxinakenty <mgreen097@gmail.com>
2018-01-14 16:17:59 +0300

    created the first stable version of devTools

---
+++ devToolsSrc/index.js
@@ -0,0 +1,5 @@
+const addButtonFor = require('./components/addButtonFor');
+
+module.exports = {
+  addButtonFor,
+};
---
+++ devToolsSrc/tools.js
@@ -0,0 +1,7 @@
+/* eslint-env browser */
+import { addButtonFor } from './';
+
+const root = document.getElementById('root');
+addButtonFor(root);
+
+console.log('yep');
