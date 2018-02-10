7c6efcd53fd56011f6c68c6d454964078e0e89a8
maxinakenty <mgreen097@gmail.com>
2018-02-10 21:09:47 +0300

    add parser for t unit

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const rootSize = require('./parser/root/rootSize');
 const baseSize = require('./parser/base/baseSize');
 const above = require('./parser/breakpoints/above');
 const nt = require('./parser/ntFunction');
-const calcFontSize = require('./api/calcFontSize');
+const { calcFontSize } = require('./api/calcFontSize');
 const {
   CONTAINS_POSITIVE_OR_NEGATIVE_FLOATING_POINT_NUMBER_AND_NT_UNIT,
-} = require('./regex/');
+} = require('./regex');
 
 const defualtConfig = {
   base: '16px',
@@ -15,58 +15,105 @@ const defualtConfig = {
   ratio: 1.333,
 };
 
-// const getFontSize = root =>
-//   root.walkDecls(decl => {
-//     let { value } = decl;
-//     const { prop } = decl;
-//     const isFontSize = prop === 'font-size';
-//     const CONTAINS_POSITIVE_OR_NEGATIVE_FLOATING_POINT_NUMBER_AND_T_UNIT = /^-?\d+(\.\d+)?t$/.test(
-//       value,
-//     );
-//     const isNumberWithTUnit = CONTAINS_POSITIVE_OR_NEGATIVE_FLOATING_POINT_NUMBER_AND_T_UNIT;
-//     const isFontSizeWithTUnit = isFontSize && isNumberWithTUnit;
-//     if (isFontSizeWithTUnit) {
-//       value = value
-//         .split('')
-//         .reverse()
-//         .join('');
-//     }
-//   });
-
 const plugin = postcss.plugin('new-typography', (config = defualtConfig) => {
   const breakpoints = calculate(config);
 
   return root => {
     root.walkDecls(decl => {
-      const { prop, value } = decl;
+      const { prop } = decl;
+      const { value } = decl;
       const isFontSizeDecl = prop === 'font-size';
       const CONTAINS_POSITIVE_OR_NEGATIVE_FLOATING_POINT_NUMBER_AND_T_UNIT = /^-?\d+(\.\d+)?t$/.test(
         value,
       );
       const isTUnit = CONTAINS_POSITIVE_OR_NEGATIVE_FLOATING_POINT_NUMBER_AND_T_UNIT;
-      const isFontSizeWithTUnit = isFontSizeDecl && isTUnit;
+      const isFontSizeDeclContainsTUnit = isFontSizeDecl && isTUnit;
 
-      if (isFontSizeWithTUnit) {
-        const target = value.replace(/\D/, '');
-        decl.value = calcFontSize(target, breakpoints);
+      const wrapper = decl.parent;
+      const { type } = wrapper;
+      const isRule = type === 'rule';
+      const isAtrule = type === 'atrule';
+      const isAbove =
+        wrapper.parent.name === 'nt-above' || wrapper.name === 'nt-above';
+      const isBelow =
+        wrapper.parent.name === 'nt-below' || wrapper.name === 'nt-below';
+
+      // Если font-size содержит единицу t
+      if (isFontSizeDeclContainsTUnit) {
+        // Если не содержит обёртки
+        if (isRule && wrapper.parent.type === 'root') {
+          const target = decl.value.replace(/\D/, '');
+          decl.value = calcFontSize(target, breakpoints);
+        }
+
+        if (
+          (isRule && isAbove) ||
+          (isAtrule && isAbove) ||
+          (isAtrule && isBelow)
+        ) {
+          const breaks = breakpoints
+            .map(b => b.name)
+            .filter(b => b !== 'default');
+          const replaceBrackets = string => string.replace(/[()]/g, '');
+          const isBreakpoint = breakpoint =>
+            breaks.some(item => item === replaceBrackets(breakpoint));
+          const isCustomBreakpoint = string =>
+            parseInt(replaceBrackets(string), 10);
+          const CONTAINS_PX = /\d+px/;
+          const CONTAINS_EM = /\d+em/;
+          const target = decl.value.replace(/\D/, '');
+
+          if (wrapper.params && isBreakpoint(wrapper.params)) {
+            const breakpointName = replaceBrackets(wrapper.params);
+            decl.value = calcFontSize(target, breakpoints, breakpointName);
+          }
+
+          if (wrapper.parent.params && isBreakpoint(wrapper.parent.params)) {
+            const breakpointName = replaceBrackets(wrapper.parent.params);
+            decl.value = calcFontSize(target, breakpoints, breakpointName);
+          }
+
+          if (wrapper.params && isCustomBreakpoint(wrapper.params)) {
+            if (CONTAINS_PX.test(wrapper.params)) {
+              console.log('contains px');
+            }
+
+            if (CONTAINS_EM.test(wrapper.params)) {
+              console.log('contains em');
+            }
+          }
+
+          if (
+            wrapper.parent.params &&
+            isCustomBreakpoint(wrapper.parent.params)
+          ) {
+            if (CONTAINS_PX.test(wrapper.parent.params)) {
+              console.log('contains px');
+            }
+
+            if (CONTAINS_EM.test(wrapper.parent.params)) {
+              console.log('contains em');
+            }
+          }
+        }
       }
     });
 
     root.walk(node => {
-      if (node.name === 'nt-root') {
-        rootSize(node, breakpoints);
-      } else if (node.name === 'nt-base') {
-        baseSize(node, breakpoints);
-      } else if (node.name === 'nt-above') {
-        above(node, breakpoints);
-      } else if (node.name === 'nt-below') {
-      } else if (node.name === 'nt-between') {
-      } else if (
-        CONTAINS_POSITIVE_OR_NEGATIVE_FLOATING_POINT_NUMBER_AND_NT_UNIT.test(
-          node.value,
-        )
-      ) {
-        nt(node, breakpoints, root);
+      switch (node.name) {
+        case 'nt-root':
+          rootSize(node, breakpoints);
+          break;
+        case 'nt-base':
+          baseSize(node, breakpoints);
+          break;
+        case 'nt-above':
+        case 'nt-below':
+        case 'nt-between':
+          break;
+
+        default:
+          break;
       }
     });
   };
