a4c013fe06e68cc2c864f30dcecc0754f648cc08
maxinakenty <mgreen097@gmail.com>
2018-02-20 21:45:02 +0300

    add transfromation ms unit measure

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 const postcss = require('postcss');
-const calculate = require('./api/store');
-const rootSize = require('./parser/root/rootSize');
-const baseSize = require('./parser/base/baseSize');
-const above = require('./parser/breakpoints/above');
-const nt = require('./parser/ntFunction');
-const { calcFontSize } = require('./api/calcFontSize');
-const {
-  CONTAINS_POSITIVE_OR_NEGATIVE_FLOATING_POINT_NUMBER_AND_NT_UNIT,
-} = require('./regex');
+const transformMsUnit = require('./lib/transformMsUnit');
 
 const defualtConfig = {
   base: '16px',
@@ -15,108 +7,13 @@ const defualtConfig = {
   ratio: 1.333,
 };
 
-const plugin = postcss.plugin('new-typography', (config = defualtConfig) => {
-  const breakpoints = calculate(config);
-
-  return root => {
-    root.walkDecls(decl => {
-      const { prop } = decl;
-      const { value } = decl;
-      const isFontSizeDecl = prop === 'font-size';
-      const CONTAINS_POSITIVE_OR_NEGATIVE_FLOATING_POINT_NUMBER_AND_T_UNIT = /^-?\d+(\.\d+)?t$/.test(
-        value,
-      );
-      const isTUnit = CONTAINS_POSITIVE_OR_NEGATIVE_FLOATING_POINT_NUMBER_AND_T_UNIT;
-      const isFontSizeDeclContainsTUnit = isFontSizeDecl && isTUnit;
-
-      const wrapper = decl.parent;
-      const { type } = wrapper;
-      const isRule = type === 'rule';
-      const isAtrule = type === 'atrule';
-      const isAbove =
-        wrapper.parent.name === 'nt-above' || wrapper.name === 'nt-above';
-      const isBelow =
-        wrapper.parent.name === 'nt-below' || wrapper.name === 'nt-below';
-
-      // Если font-size содержит единицу t
-      if (isFontSizeDeclContainsTUnit) {
-        // Если не содержит обёртки
-        if (isRule && wrapper.parent.type === 'root') {
-          const target = decl.value.replace(/\D/, '');
-          decl.value = calcFontSize(target, breakpoints);
-        }
-
-        if (
-          (isRule && isAbove) ||
-          (isAtrule && isAbove) ||
-          (isAtrule && isBelow)
-        ) {
-          const breaks = breakpoints
-            .map(b => b.name)
-            .filter(b => b !== 'default');
-          const replaceBrackets = string => string.replace(/[()]/g, '');
-          const isBreakpoint = breakpoint =>
-            breaks.some(item => item === replaceBrackets(breakpoint));
-          const isCustomBreakpoint = string =>
-            parseInt(replaceBrackets(string), 10);
-          const CONTAINS_PX = /\d+px/;
-          const CONTAINS_EM = /\d+em/;
-          const target = decl.value.replace(/\D/, '');
-
-          if (wrapper.params && isBreakpoint(wrapper.params)) {
-            const breakpointName = replaceBrackets(wrapper.params);
-            decl.value = calcFontSize(target, breakpoints, breakpointName);
-          }
-
-          if (wrapper.parent.params && isBreakpoint(wrapper.parent.params)) {
-            const breakpointName = replaceBrackets(wrapper.parent.params);
-            decl.value = calcFontSize(target, breakpoints, breakpointName);
-          }
-
-          if (wrapper.params && isCustomBreakpoint(wrapper.params)) {
-            if (CONTAINS_PX.test(wrapper.params)) {
-              console.log('contains px');
-            }
-
-            if (CONTAINS_EM.test(wrapper.params)) {
-              console.log('contains em');
-            }
-          }
-
-          if (
-            wrapper.parent.params &&
-            isCustomBreakpoint(wrapper.parent.params)
-          ) {
-            if (CONTAINS_PX.test(wrapper.parent.params)) {
-              console.log('contains px');
-            }
-
-            if (CONTAINS_EM.test(wrapper.parent.params)) {
-              console.log('contains em');
-            }
-          }
-        }
-      }
-    });
-
+const plugin = postcss.plugin(
+  'new-typography',
+  (config = defualtConfig) => root => {
     root.walk(node => {
-      switch (node.name) {
-        case 'nt-root':
-          rootSize(node, breakpoints);
-          break;
-        case 'nt-base':
-          baseSize(node, breakpoints);
-          break;
-        case 'nt-above':
-        case 'nt-below':
-        case 'nt-between':
-          break;
-
-        default:
-          break;
-      }
+      if (transformMsUnit.test(node)) transformMsUnit(node, config);
     });
-  };
-});
+  },
+);
 
 module.exports = plugin;
