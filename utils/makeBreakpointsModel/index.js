6326506a49d1774b63e52d9f1730c00299d70e16
maxinakenty <mgreen097@gmail.com>
2018-03-17 09:53:12 +0300

    add comments

--- utils/makeBreakpointsModel/index.js
+++ utils/makeBreakpointsModel/index.js
@@ -15,17 +15,14 @@ const isValidUserConfig = require('../isValidUserConfig');
  * @return {Array<Object>} An array containing object of the default breakpoint.
  */
 const makeFirstBreakpoint = config =>
-  [{}]
-    .map(item => (
-      {
-        ...item,
-        base: config.base,
-        lineHeight: config.lineHeight,
-        ratio: config.ratio,
-        value: '0px',
-        name: 'default',
-      }
-    ));
+  [{}].map(item => ({
+    ...item,
+    base: config.base,
+    lineHeight: config.lineHeight,
+    ratio: config.ratio,
+    value: '0px',
+    name: 'default',
+  }));
 
 /**
  * @param {Object} config User configuration.
@@ -41,20 +38,21 @@ const getNamesOfBreakpoints = config =>
  * @param {Function} fn getNamesOfBreakpoints function.
  * @return {Array<Object>} An array with objects of breakpoints, not including the first breakpoint.
  */
-const makeBreakpoints = (config) => {
+const makeBreakpoints = config => {
   const namesOfBreakpoints = getNamesOfBreakpoints(config);
 
-  return Object.values(config)
-    .filter(b => isObject(b) && b.breakpoint)
-    .map((b, i) => (
-      { ...b,
+  return (
+    Object.values(config)
+      .filter(b => isObject(b) && b.breakpoint)
+      .map((b, i) => ({
+        ...b,
         name: namesOfBreakpoints[i],
         value: b.breakpoint,
       }))
-      // Remove breakpoint key.
-    .map(b => omit(b, 'breakpoint'));
+      // Remove the key with the name breakpoint.
+      .map(b => omit(b, 'breakpoint'))
+  );
 };
-
 
 /**
  * @param {string} value The value of the breakpoint in pixels or em.
@@ -63,13 +61,19 @@ const makeBreakpoints = (config) => {
 const toPxValueOfBreakpoint = value =>
   HAS_EM.test(value) ? toPx(value) : value;
 
+/**
+ * The function creates an array with an object for each breakpoint.
+ * If there is no value in the current breakpoint, for example, the ratio,
+ * we inherit it from the previous breakpoint. Calculate the value of the ratio
+ *if it was specified as a string with the "at" parameter (example: "45px at 6").
+ * Convert the value of breakpoint to pixels, if it is specified in em. We add and calculate the value of root.
+ *
+ * @param {Object} config User configuration.
+ */
 const makeBreakpointsModel = config => {
   if (!isValidUserConfig(config)) return null;
 
-  return [
-    ...makeFirstBreakpoint(config),
-    ...makeBreakpoints(config),
-  ]
+  return [...makeFirstBreakpoint(config), ...makeBreakpoints(config)]
     .reduce((breakpoint, item, i) => {
       const stripBase = stripUnit(item.base);
 
@@ -79,8 +83,8 @@ const makeBreakpointsModel = config => {
           ...item,
           base: !item.base ? breakpoint[i - 1].base : stripBase,
           lineHeight: !item.lineHeight
-          ? breakpoint[i - 1].lineHeight
-          : item.lineHeight,
+            ? breakpoint[i - 1].lineHeight
+            : item.lineHeight,
           ratio: !item.ratio
             ? breakpoint[i - 1].ratio
             : getRatio(item.ratio, stripBase),
