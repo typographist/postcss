106263b53bf8d72b99b160932ca1c38929f36771
maxinakenty <mgreen097@gmail.com>
2018-03-21 23:58:49 +0300

    add transfomTOnly.test.js

---
+++ transformator/transformAtrules/transformTOnly/transformTOnly.test.js
@@ -0,0 +1,24 @@
+const run = require('../../../helpersForTests/run');
+const { userConfig } = require('../../../helpersForTests/mocks');
+
+describe('transformator', () => {
+  describe('@t-only', () => {
+    it('should replace @t-only(desktop) with @media screen and (min-width: 48em) and (max-width: 61.99875em)', () => {
+      const source = `
+        .test {
+          @t-only (desktop) {
+            background-color: rebeccapurple;
+          }
+        }`;
+
+      const compiled = `
+        @media screen and (min-width: 48em) and (max-width: 61.99875em) {
+    .test {
+        background-color: rebeccapurple;
+    }
+}`;
+
+      return run(source, compiled, userConfig);
+    });
+  });
+});
