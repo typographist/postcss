b0ae0a33797e13f03df10f56d8a8f5b3781e2db9
maxinakenty <mgreen097@gmail.com>
2018-04-07 20:51:04 +0300

    add devtools

---
+++ reactDevtools/index.js
@@ -0,0 +1,11 @@
+import verticalRhythm from './_source/src/ducks/verticalRhythm';
+import excludeReducer from './_source/src/excludeReducer';
+import RhythmToggleButton from './_source/src/containers/RhythmToggleButton';
+import Typographist from './_source/src/containers/Typographist';
+
+module.exports = {
+  verticalRhythm,
+  excludeReducer,
+  RhythmToggleButton,
+  Typographist,
+};
