c3c6b1300565718d59c052b1e8b97ac5800dd9e7
maxinakenty <mgreen097@gmail.com>
2018-07-13 18:42:19 +0300

    add tests for t-above

--- transformator/transformAtrules/transformTAbove/transformTAbove.spec.js
+++ transformator/transformAtrules/transformTAbove/transformTAbove.spec.js
@@ -2,7 +2,7 @@ const run = require('../../../helpersForTests/run');
 const { userConfig } = require('../../../helpersForTests/mocks');
 
 describe('transformator', () => {
-  describe('@t-above', () => {
+  describe('@t-above atrule', () => {
     it('should replace @t-above(desktop) with @media (min-width: 48em)', () => {
       const source = `
         .test {
@@ -51,6 +51,44 @@ describe('transformator', () => {
         background-color: orange;
     }
 }`;
+      return run(source, compiled, userConfig);
+    });
+  });
+
+  describe('@t-above atrule with orientation', () => {
+    it('should replace @t-above(40em):landscape with @media (min-with: 40em) and (orientation: landscape)', () => {
+      const source = `
+      .test {
+        @t-above(40em):landscape {
+          background-color: orange;
+        }
+      }`;
+
+      const compiled = `
+      @media (min-width: 40em) and (orientation: landscape) {
+    .test {
+        background-color: orange;
+    }
+}`;
+
+      return run(source, compiled, userConfig);
+    });
+
+    it('should replace @t-above(40em):portrait with @media (min-with: 40em) and (orientation: portrait)', () => {
+      const source = `
+      .test {
+        @t-above(40em):portrait {
+          background-color: orange;
+        }
+      }`;
+
+      const compiled = `
+      @media (min-width: 40em) and (orientation: portrait) {
+    .test {
+        background-color: orange;
+    }
+}`;
+
       return run(source, compiled, userConfig);
     });
   });
