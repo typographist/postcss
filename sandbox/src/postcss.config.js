ffced73430b6d1c22efd008be1d83217bddea7d1
mg901 <mgreen097@gmail.com>
2019-07-21 09:59:17 +0300

    chore: add sandbox

---
+++ sandbox/src/postcss.config.js
@@ -0,0 +1,25 @@
+const { typographist, ratios } = require('../../index.js');
+
+module.exports = () => ({
+  plugins: [
+    typographist({
+      base: '16px',
+      lineHeight: 1.5,
+      ratio: ratios.MINOR_SECOND,
+      tablet: {
+        breakpoint: '768px',
+        base: '17px',
+        ratio: ratios.MAJOR_SECOND,
+      },
+      desktop: {
+        breakpoint: '992px',
+        base: '19px',
+        ratio: ratios.MINOR_THIRD,
+      },
+      lgDesktop: {
+        breakpoint: '1200px',
+        base: '21px',
+      },
+    }),
+  ],
+});
