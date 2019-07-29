5f0ed46f9c02b340b8e711064da81dd5e97d33df
mg901 <mgreen097@gmail.com>
2019-07-29 14:22:55 +0300

    refactor: added check for 2 arguments to the function between

--- breakpoints/validators.js
+++ breakpoints/validators.js
@@ -9,6 +9,7 @@ const {
 } = require('./helpers');
 
 const HAS_PX_OR_EM = /\d+(\.\d+)?(px|em)/;
+const PREFIX = '[typographist]:';
 
 // hasPxOrEm :: String -> Boolean
 const hasPxOrEm = (x) => HAS_PX_OR_EM.test(x);
@@ -20,24 +21,31 @@ const validateOrientation = (atrule) => {
 
   if (!isValid) {
     throw atrule.error(
-      `[typographist]: '${orientation}' is invalid orientation. Use 'portrait' or 'landscape'. Example: @up(tablet):portrait.`,
+      `${PREFIX} '${orientation}' is invalid orientation. Use 'portrait' or 'landscape'. Example: @up(tablet):portrait.`,
     );
   }
 };
 
 // validateBreakpointValue ::  (Object, Object) -> Void
-const validateBreakpointValue = (breakpoints, atrule) =>
+const validateBreakpointValue = (breakpoints, atrule) => {
   getBreakpointValues(atrule.params).forEach((p) => {
+    if (/[A-Z]/.test(p)) {
+      throw atrule.error(
+        `${PREFIX} '${p}' is invalid breakpoint name. Set the name to lowercase, use kebab case notation. Example: 'desktop' or 'lg-desktop'.`,
+      );
+    }
+
     const isValidValue = !!breakpoints[camelize(p)] || hasPxOrEm(p);
 
-    if (isValidValue) {
+    if (!isValidValue) {
       throw atrule.error(
-        `[typographist]: '${p}' is invalid breakpoint name. Use '${makeBreakpointList(
+        `${PREFIX} '${p}' is invalid breakpoint name. Use '${makeBreakpointList(
           breakpoints,
         )}' or values with pixels or ems. Example: @up(tablet) or @up(1200px) or @up(60em).`,
       );
     }
   });
+};
 
 // lastBreakpointOrNot :: (Object, Object) -> Void
 const lastBreakpointOrNot = (breakpoints, atrule) => {
@@ -47,7 +55,17 @@ const lastBreakpointOrNot = (breakpoints, atrule) => {
 
   if (currentIndex === lastBreakIndex) {
     throw atrule.error(
-      `[typographist]: Don't use '${breakpointName}' because it doesn't have a maximum width.`,
+      `${PREFIX} Don't use '${breakpointName}' because it doesn't have a maximum width.`,
+    );
+  }
+};
+
+const hasTwoArguments = ({ params }) => {
+  const args = getBreakpointValues(params).length;
+
+  if (args < 2) {
+    throw new Error(
+      `${PREFIX} '@between' must have two arguments. The first is the minimum breakpoint, the second is the maximum break point.'`,
     );
   }
 };
@@ -57,4 +75,5 @@ module.exports = {
   validateBreakpointValue,
   lastBreakpointOrNot,
   hasPxOrEm,
+  hasTwoArguments,
 };
